f1812131df668f326dc5a772067bde9d0d76bd26
satori8 <s.tocheny@gmail.com>
2015-09-22 05:44:38 +0300

    init

---
+++ catalog/view/theme/default/template/common/search.tpl
@@ -0,0 +1,6 @@
+<div id="search" class="input-group">
+  <input type="text" name="search" value="<?php echo $search; ?>" placeholder="<?php echo $text_search; ?>" class="form-control input-lg search-field" />
+  <span class="input-group-btn search-button">
+    <button type="button" class="btn btn-default btn-lg"><i class="fa fa-search"></i></button>
+  </span>
+</div>
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/OpenCart/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/OpenCart/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/OpenCart/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/OpenCart/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/OpenCart/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/OpenCart/www/image/');
+define('DIR_CACHE', 'Z:/home/OpenCart/www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/OpenCart/www/system/download/');
+define('DIR_UPLOAD', 'Z:/home/OpenCart/www/system/upload/');
+define('DIR_MODIFICATION', 'Z:/home/OpenCart/www/system/modification/');
+define('DIR_LOGS', 'Z:/home/OpenCart/www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
